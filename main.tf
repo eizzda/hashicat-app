43993ed024b0c1ee9f0e90a0e98dfcf67ac0d8c4
eizzda <adamz.zhang@gmail.com>
2023-08-23 04:13:54 +0000

    use private module

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "aws_key_pair" "hashicat" {
 }
 
 module "s3_bucket" {
-  source              = "app.terraform.io/terraform-workshop-august/s3-bucket/aws"
-  version             = "3.1.0"
+  source              = "app.terraform.io/terraform-workshop-august/s3-bucket-terraform-workshop/aws"
+  version             = "3.1.3"
   s3_object_ownership = "BucketOwnerEnforced"
 }
